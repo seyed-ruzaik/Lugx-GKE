d7e9f1eb9d7bc54c4500a12d2f35adb33b8a5648
Seyed Ruzaik <seyedruzaik@gmail.com>
2025-07-06 08:11:20 +0530

    Setup GKE deployment workflow

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -6,7 +6,7 @@ def test_order_service_get():
     Test GET request to /orders endpoint of order service.
     """
     try:
-        res = requests.get("http://127.0.0.1:5001/orders")
+        res = requests.get("http://34.93.184.130:5001/orders")
         assert res.status_code == 200
         print("Order Service GET OK")
     except Exception as e:
@@ -22,7 +22,7 @@ def test_order_service_post():
             "items": ["Call of Duty 2", "FIFA 25"],
             "total": 120.98
         }
-        res = requests.post("http://127.0.0.1:5001/place-order", json=payload)
+        res = requests.post("http://34.93.184.130:5001/place-order", json=payload)
         assert res.status_code == 201
         print("Order Service POST OK")
     except Exception as e:
@@ -34,7 +34,7 @@ def test_game_service_get():
     Test GET request to /games endpoint of game service.
     """
     try:
-        res = requests.get("http://127.0.0.1:5000/games")
+        res = requests.get("http://34.100.182.195:5000/games")
         assert res.status_code == 200
         print("Game Service GET OK")
     except Exception as e:
@@ -51,7 +51,7 @@ def test_game_service_post():
             "release_date": "2022-11-13",
             "price": 69.99
         }
-        res = requests.post("http://127.0.0.1:5000/add-game", json=payload)
+        res = requests.post("http://34.100.182.195:5000/add-game", json=payload)
         assert res.status_code == 201
         print("Game Service POST OK")
     except Exception as e:
@@ -67,7 +67,7 @@ def test_analytics_service():
             "event_type": "integration_test",
             "page_url": "/test"
         }
-        res = requests.post("http://127.0.0.1:5002/track", json=payload)
+        res = requests.post("http://35.200.226.132:5002/track", json=payload)
         assert res.status_code in [200, 201]
         print("Analytics Service Track OK")
     except Exception as e:
@@ -79,7 +79,7 @@ def test_frontend():
     Test the frontend availability.
     """
     try:
-        res = requests.get("http://127.0.0.1:8080")
+        res = requests.get("http://35.244.41.165/")
         assert res.status_code == 200
         print("Frontend OK")
     except Exception as e:
